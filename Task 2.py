667db8e952fc76170ccb31a14699fa3e5fb1882f
Kashif Akhter <146735547+iamkashifakhter@users.noreply.github.com>
2025-07-12 19:20:17 +0530

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,3 +1,3 @@
 first_name= input("Enter your first nane: ")
 second_name= input("Enter your second nane: ")
-print("Hello", first_name, second_name, ", How are you ?")
+print("Hello", first_name, second_name, ", Welcome to the Python program.")
